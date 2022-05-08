9bc28c8310eea93ebe0e0413ad29bf5070c3a2aa
shreyanshi89 <90604000+shreyanshi89@users.noreply.github.com>
2022-05-08 15:18:28 +0530

    Update Q3.java

--- LearningJava/src/Assignment_01/Q3.java
+++ LearningJava/src/Assignment_01/Q3.java
@@ -1,1 +1,35 @@
+// Given an interface in1 which includes a method display which takes
+// an integer as input .
+// interface In1
+// {
+// void display(int p);
+// }
+// Task is to write a class testClass which implements interface In1 and has
+// a method named display which takes an integer as an input p and the
+// display method should be able to tell if the number is prime or not?
+// - The main method should not be in testClass, create a separate class for
+// that.
 
+interface In1
+{
+void display(int p);
+}
+
+class testClass implements In1{
+  public void display(int p){
+    for(int i=2;i<p;i++){
+      if(p%i==0){
+        System.out.println("Not a prime number");
+        return;
+      }
+    }
+    System.out.println("Prime number");
+  }
+}
+
+class Q3{
+  public static void main(String args[]){
+    testClass obj= new testClass();
+    obj.display(7);
+  }
+}
