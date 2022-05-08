c3e0a5b6e6f94adbc9d61d580edbc9aa4c5d3e04
shreyanshi89 <90604000+shreyanshi89@users.noreply.github.com>
2022-05-08 15:34:43 +0530

    Update Q9.java

--- LearningJava/src/Assignment_01/Q9.java
+++ LearningJava/src/Assignment_01/Q9.java
@@ -1,1 +1,25 @@
+/*
+	WAP in java, which would demonstrate access protection over a class and a variable of that class.
+ 	Note: class can be public and default while variable can be default, private, protected, and public.
+ 	(If required then do create more than 2 packages).
+ 
+ */
+package Assignment_01;
+
+public class Q9 {
+	public static void main(String[] args) {
+		
+		
+		
+		Name1 obj1 = new Name1();
+		System.out.println("Accessed public variable of other class(Same Package) : "+obj1.a);		// Accessed public variable of other class 
+		System.out.println("Accessed protected variable of other class(Same Package) : "+obj1.b);		// Accessed protected variable of other class
+//		Refer Q9_Extented class
+		
+//		System.out.println(obj.c);
+		System.out.println("Can't access private vaiable");
+		
+	}
+}
+
 
